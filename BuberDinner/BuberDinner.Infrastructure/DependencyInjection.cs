114634a0bf66a360595bbfbd3e97b11b92aa164f
Patrik Fumić <patrik.fumic@gmail.com>
2023-01-09 23:25:08 +0100

    Repository pattern added

--- BuberDinner/BuberDinner.Infrastructure/DependencyInjection.cs
+++ BuberDinner/BuberDinner.Infrastructure/DependencyInjection.cs
@@ -1,7 +1,9 @@
 using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Persistance;
 using BuberDinner.Application.Common.Interfaces.Services;
 using BuberDinner.Application.Services.Authentication;
 using BuberDinner.Infrastructure.Authentication;
+using BuberDinner.Infrastructure.Persistence;
 using BuberDinner.Infrastructure.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,9 +17,12 @@ public static class DependencyInjection
         ConfigurationManager configuration)
     {
         services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
-        
+
         services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
         services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+
+        services.AddScoped<IUserRepository, UserRepository>();
+        
         return services;
     }
 }
---
+++ BuberDinner/Docs/DomainModels.md
@@ -0,0 +1,13 @@
+# Domain models
+
+## User
+
+```json
+{
+    "id": "00000000-0000-0000-0000-000000000000",
+    "firstName": "Patrik",
+    "lastName": "Fumic",
+    "email": "patrik.fumic@gmail.com",
+    "password": "Password123!"
+}
+```
