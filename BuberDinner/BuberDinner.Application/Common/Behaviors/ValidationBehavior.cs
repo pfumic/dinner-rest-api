bb24f74ce2c9f70b924a441e428483fd188e28cc
Patrik Fumić <patrik.fumic@gmail.com>
2023-01-15 23:37:19 +0100

    Validation with FluentValidation added

---
+++ BuberDinner/BuberDinner.Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,44 @@
+using BuberDinner.Application.Authentication.Commands.Register;
+using BuberDinner.Application.Authentication.Common;
+using ErrorOr;
+using FluentValidation;
+using MediatR;
+
+namespace BuberDinner.Application.Common.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    where TResponse : IErrorOr
+{
+    private readonly IValidator<TRequest>? _validator;
+
+    public ValidationBehavior(IValidator<TRequest>? validator = null)
+    {
+        _validator = validator;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (_validator is null)
+        {
+            return await next();
+        }
+
+        var validatonResult = await _validator.ValidateAsync(request, cancellationToken);
+
+        if (validatonResult.IsValid)
+        {
+            return await next();
+        }
+
+        var errors = validatonResult.Errors
+            .ConvertAll(validationFailure => Error.Validation(
+                validationFailure.PropertyName,
+                validationFailure.ErrorMessage));
+
+        return (dynamic)errors;
+    }
+}
